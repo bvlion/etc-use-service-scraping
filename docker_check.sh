d04f8848766bbcf3e3ec618c4fda40fbaba57ceb
bvlion <bvltiggeari@ambitious-i.net>
2023-03-24 14:42:10 +0900

    Change a command `docker compose` to `docker-compose`

--- docker_check.sh
+++ docker_check.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+docker-compose ps
 while :
 do
-  PS_COUNT=`docker compose ps | grep -c running`
+  PS_COUNT=`docker-compose ps | grep -c Up`
   sleep 1
   if [ $PS_COUNT -lt 2 ]; then
-    docker compose down
+    docker-compose down
     break
   fi
 done
